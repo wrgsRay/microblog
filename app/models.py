3fb7238db0d2914df02d81b7403f7164e0f3fec6
Raymond on OSX <28991499+wrgsRay@users.noreply.github.com>
2018-08-22 12:47:03 -0700

    password hash models

--- app/models.py
+++ app/models.py
@@ -19,6 +19,7 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
