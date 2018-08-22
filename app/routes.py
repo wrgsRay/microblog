d90acc68ec100cd4515d0453b23286e22b5f54a6
Raymond on OSX <28991499+wrgsRay@users.noreply.github.com>
2018-08-22 13:45:47 -0700

    logout implemented

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from app.forms import LoginForm
 from flask import flash, redirect, url_for
 from app.models import User
@@ -36,3 +36,9 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign in', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
