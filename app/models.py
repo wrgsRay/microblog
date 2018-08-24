6cfd7c5da124f6e9b42d0150a3c0b24c822c2adc
Raymond on OSX <28991499+wrgsRay@users.noreply.github.com>
2018-08-23 22:25:06 -0700

    last seen implemented

--- app/models.py
+++ app/models.py
@@ -11,6 +11,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -37,5 +39,5 @@ class Post(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __reduce__(self):
+    def __repr__(self):
         return '<Post {}>'.format(self.body)
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,17 @@
 from app import app, db
+from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app.forms import LoginForm, RegistrationForm
 from werkzeug.urls import url_parse
 from app.models import User
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 @app.route('/')
